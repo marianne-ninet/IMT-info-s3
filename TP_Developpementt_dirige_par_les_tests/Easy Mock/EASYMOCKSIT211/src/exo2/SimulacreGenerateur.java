2912f5850858404bd1f8b9fe9ac308a1c9d3b14d
Marianne Ninet <marianne@MacBook-Pro-de-Marianne-2.local>
2024-09-14 20:42:17 +0200

    Supprimer le dossier TP_Developpementt_dirige_par_les_tests

--- TP_Developpementt_dirige_par_les_tests/Easy Mock/EASYMOCKSIT211/src/exo2/SimulacreGenerateur.java
+++
@@ -1,9 +0,0 @@
-package exo2;
-
-public class SimulacreGenerateur implements IGenerateur {
-
-    @Override
-    public int un_entier() {
-        return 2;  // Retourne directement 2 pour le test.
-    }
-}
